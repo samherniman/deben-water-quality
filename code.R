c5de54f10464ae1a881b173a18b10f408606d26f
Sam Herniman <samherniman@posteo.net>
2024-08-13 21:27:26 +0100

    unique w3w

--- code.R
+++ code.R
@@ -1,11 +1,26 @@
 source(here::here("fxns.R"))
 
+# Combine all excel sheets into one --------------------------------------
 df <- 
   fs::dir_ls(
     here::here("data/raw"),
     regexp = "xlsx"
   ) |>  
-lapply(xl_paths, convert_to_csv) |> 
+lapply(convert_to_csv) |> 
 lapply(data.table::fread)
 
 df <- do.call(rbind, df)
+vroom::vroom_write(
+  df, 
+  here::here("data/working/working.csv"), 
+  append = FALSE,
+  delim = ","
+)
+
+# Find unique w3w addresses ----------------------------------------------
+
+
+tbl <- unique(df$what3words_location) |> tibble::as_tibble()
+
+vroom::vroom_write(tbl, here::here("data/working/unique_w3w.csv"))
+
